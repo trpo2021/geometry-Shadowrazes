8a3f1c8371e7440fad245fa0b503920b04844dad
Shadowrazes <hugehug@mail.ru>
2021-02-20 01:19:23 +0700

    add input/output

---
+++ geometry.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "RUSSIAN");
+    vector<string> list;
+    string s;
+    int i;
+    cout << "Задайте фигуры" << endl;
+    while (getline(cin, s)) {
+        if (s == "")
+        {
+            break;
+        }
+        list.push_back(s);
+    }
+    for (i = 0; i < list.size(); i++) 
+    {
+        cout << list[i] << endl;
+    }
+}
