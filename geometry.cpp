043f230b22949240c4d22b2d42d0ebcd4a8731fe
Shadowrazes <hugehug@mail.ru>
2021-02-20 01:21:27 +0700

    add procedure for extract the coordinates of a triangle

--- geometry.cpp
+++ geometry.cpp
@@ -6,6 +6,43 @@
 
 
 using namespace std;
+const int T = 8;
+
+void triangle(string& s)
+{
+    double mas[T];
+    string t = "";
+    int k = 0;
+    if (s.find("((") < s.find("))"))
+    {
+        for (int i = s.find("((") + 2; i <= s.find("))"); i++)
+        {
+            if (s[i] == 32 || s[i] == 44 || s[i] == 41) //пробел || запятая || скобка
+            {
+                if (t != "")
+                {
+                    mas[k] = stod(t);
+                    k++;
+                    if (k == T)
+                    {
+                        for (int i = 0; i < T; i++)
+                            cout << mas[i] << " ";
+                        cout << endl;
+                        return;
+                    }
+                }
+                t = "";
+                continue;
+            }
+            if ((s[i] < 47 || s[i]>58) && (s[i] != 45 && s[i] != 46))
+            {
+                cout << "error" << endl;
+                return;
+            }
+            t += s[i];
+        }
+    }
+}
 
 int main()
 {
