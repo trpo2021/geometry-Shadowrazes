50fa58af69561b7cde2068b201a84061938cdba0
Shadowrazes <hugehug@mail.ru>
2021-02-23 16:42:09 +0700

    add code list for errors

--- geometry.cpp
+++ geometry.cpp
@@ -44,6 +44,32 @@ void triangle(string& s)
     }
 }
 
+void error(int code, int column)
+{
+    for (int i = 0; i < column; i++) {
+        cout << " ";
+    }
+    cout << "^" << endl;
+
+    switch (code) {
+    case 1:
+        cout << "Error at column " << column << ": expected 'triangle'; code = " << code << endl;
+        break;
+    case 2:
+        cout << "Error at column " << column << ": expected '<double>'; code = " << code << endl;
+        break;
+    case 3:
+        cout << "Error at column " << column << ": expected ')'; code = " << code << endl;
+        break;
+    case 4:
+        cout << "Error at column " << column << ": unexpected token; code = " << code << endl;
+        break;
+    case 5:
+        cout << "Error at column " << column << ": expected '('; code = " << code << endl;
+        break;
+    }
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "RUSSIAN");
