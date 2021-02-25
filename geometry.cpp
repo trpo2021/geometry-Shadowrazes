813776f69819d83d85a62a4baff36c47b4f63a39
Shadowrazes <hugehug@mail.ru>
2021-02-26 00:55:35 +0700

    update description of the errors

--- geometry.cpp
+++ geometry.cpp
@@ -18,19 +18,19 @@ void error(int code, int column)
     switch (code)
     {
     case 1:
-        cout << "Error at column " << column << ": expected 'triangle'; code = " << code << endl;
+        cout << "Error at column " << column << ": 'circle', 'triangle' or 'polygon'; code = " << code << endl;
         break;
     case 2:
         cout << "Error at column " << column << ": expected '<double>'; code = " << code << endl;
         break;
     case 3:
-        cout << "Error at column " << column << ": expected ')'; code = " << code << endl;
+        cout << "Error at column " << column << ": expected '))'; code = " << code << endl;
         break;
     case 4:
         cout << "Error at column " << column << ": unexpected token; code = " << code << endl;
         break;
     case 5:
-        cout << "Error at column " << column << ": expected '('; code = " << code << endl;
+        cout << "Error at column " << column << ": expected '(('; code = " << code << endl;
         break;
     }
 }
