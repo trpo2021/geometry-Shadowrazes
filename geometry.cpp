ad2b527d981e79fcff2052e67a20b48b67e483b2
Shadowrazes <hugehug@mail.ru>
2021-03-18 23:23:15 +0700

    add output

--- geometry.cpp
+++ geometry.cpp
@@ -625,8 +625,13 @@ int main()
 {
     setlocale(LC_CTYPE, "RUSSIAN");
     vector<pair<string, vector<double>>> flist;
+    vector <string> str;
     string s;
-    int i, j;
+
+    pair <double, double> start; pair <double, double> close;    
+    vector <double> leng;
+
+    int i, j, k;
     cout << "Задайте фигуры" << endl;
     while (getline(cin, s))
     {
@@ -655,20 +660,52 @@ int main()
 
         if (coord.size() > 0)
         {
+            str.push_back(s);      
             figure.first = name;
             figure.second = coord;
             flist.push_back(figure);
         }
     }
+
+    vector <vector<int>> inter;
+    inter.resize(flist.size());
+
     int z = flist.size();
+    inter = intersects(flist, inter);
     for (i = 0; i < z; i++)
     {
-        cout << i + 1 << ". " << flist[i].first << ": ";
-        int x = flist[i].second.size();
-        for (j = 0; j < x; j++)
+        leng.clear();
+        double perimeter = 0, square = 0;
+        if (flist[i].first == "triangle")                       
         {
-            cout << flist[i].second[j] << " ";
+            for (j = 0; j < 6; j += 2)
+            {
+                start.first = flist[i].second[j]; start.second = flist[i].second[j + 1];
+                close.first = flist[i].second[j + 2]; close.second = flist[i].second[j + 3];
+                leng.push_back(lenLine(start, close));
+            }
+            for (j = 0; j < 3; j++) perimeter += leng[j];
+            square = sqrTriangle(perimeter / 2, leng);
         }
-        cout << endl;
+        else if (flist[i].first == "circle")
+        {
+            perimeter = lenCircle(flist[i].second[2]);
+            square = sqrCircle(flist[i].second[2]);
+        }
+        cout << i + 1 << ". " << str[i] << endl;       
+        cout << "    perimeter = " << perimeter << endl;
+        cout << "    area = " << square << endl;
+        cout << "    intersects:" << endl;
+        int m = inter[i].size();
+        if (m > 0)
+        {
+            for (j = 0; j < m; j++)
+            {
+                cout << "      " << inter[i][j] + 1 << ". " << flist[inter[i][j]].first << endl;
+            }
+            cout << endl;
+        }
+        else
+            cout << "       none" << endl << endl;
     }
 }
