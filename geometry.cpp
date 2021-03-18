e435eb2e90312d0a4b0ec02641688920a65af00b
Shadowrazes <hugehug@mail.ru>
2021-03-18 23:02:06 +0700

    add function lenLine

--- geometry.cpp
+++ geometry.cpp
@@ -265,6 +265,13 @@ pair <double, double> vectCoord(pair <double, double> start, pair <double, doubl
     return vect;
 }
 
+double lenLine(pair <double, double> start, pair <double, double> close) 
+{
+    pair <double, double> vect = vectCoord(start, close);
+    double leng = sqrt(pow(vect.first, 2) + pow(vect.second, 2));
+    return leng;
+}
+
 vector <double> setCircle(string& s)
 {
     vector<double> coord;
